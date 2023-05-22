e1eb2b9f6bbad16a10004f181048f7f48df8c49f
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2023-05-22 09:27:06 +0530

    setup edit

--- setup_py_env.sh
+++ setup_py_env.sh
@@ -2,6 +2,7 @@ mkdir -p data
 cd data
 curl -LO https://datahub.io/machine-learning/credit-g/r/credit-g.json
 cd ..
+apt install python3-venv 
 python3 -m venv venv
 source venv/bin/activate
 pip install -r src/requirements.txt  
